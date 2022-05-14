899d1b5780412491698d4a1f0d2cb2e029208049
motokikando <kando813motoki@gmail.com>
2022-05-14 18:47:05 +0900

    update

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+const API_URL = 'https://www.boredapi.com/api/activity/'
+
 
 
 
 
 function getAPI(){
-
+    fetch(API_URL)
+    .then(response => response.json())
+    .then(data => data.activity)
 }
+
+getAPI();
