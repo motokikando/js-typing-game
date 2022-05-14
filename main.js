a3f7f3aeeaa685577515b7f2c88dce3b47982ecb
motokikando <kando813motoki@gmail.com>
2022-05-14 19:58:57 +0900

    update

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const API_URL = 'https://www.boredapi.com/api/activity/'
+const inputElement = document.getElementById('inputSentence')
+const diplayElement = document.getElementById('sentenceDisplay')
+
+
+
+
 
 
 
@@ -10,4 +16,13 @@ function getAPI(){
     .then(data => data.activity)
 }
 
-getAPI();
+//非同期処理
+async function renderNewSentence(){
+    // 実行完了まで待つ非同期関数を定義
+    const sentence = await getAPI() // 呼び出した文字列を格納
+    sentence.split('').forEach( char => {
+        const charSpan = document.createElement('span') //spanタグの作成
+        charSpan.innerText = char //spanタグ内のテキストにchar文字を挿入
+    })
+    inputElement.value = null
+}
