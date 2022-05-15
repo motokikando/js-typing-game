d62929015c5ff3a40d9dc2fd8d4a32ba6666c3c7
motokikando <kando813motoki@gmail.com>
2022-05-15 16:21:44 +0900

    update

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
 const API_URL = 'https://www.boredapi.com/api/activity/'
-const inputElement = document.getElementById('inputSentence')
+const inputElement= document.getElementById('inputText')
 const diplayElement = document.getElementById('sentenceDisplay')
 
 
 
+
+//入力操作
+inputElement.addEventListener('input', () => {
+    let correct = true
+    //spanタグを全て取得してリストにする。
+    const arrayAPI = diplayElement.querySelectorAll('span')
+    console.log(arrayAPI)
+    //split('')で入力した文字列を配列として管理
+    const arrayValue = inputElement.value.split('')
+    //arrayAPIの文字とarrayValueの判定
+    arrayAPI.forEach((charSpan, index) => {
+        const input_char = arrayValue[index]
+        if (input_char == null){
+            charSpan.classList.remove('correct')
+            charSpan.classList.remove('incorrect')
+            correct = false
+        } else if( input_char === charSpan.innerText){ //入力一致
+            charSpan.classList.add('correct')
+            charSpan.classList.remove('incorrect')
+        } else { //入力ミス
+            charSpan.classList.remove('correct')
+            charSpan.classList.add('incorrect')
+            correct = false
+        }
+    })
+    if (correct){renderNewSentence()}
+})
 
 
 
@@ -11,7 +38,7 @@ const diplayElement = document.getElementById('sentenceDisplay')
 
 
 function getAPI(){
-    fetch(API_URL)
+    return fetch(API_URL)
     .then(response => response.json())
     .then(data => data.activity)
 }
@@ -20,9 +47,14 @@ function getAPI(){
 async function renderNewSentence(){
     // 実行完了まで待つ非同期関数を定義
     const sentence = await getAPI() // 呼び出した文字列を格納
+    diplayElement.innerHTML = ''
     sentence.split('').forEach( char => {
         const charSpan = document.createElement('span') //spanタグの作成
         charSpan.innerText = char //spanタグ内のテキストにchar文字を挿入
+        diplayElement.appendChild(charSpan)
     })
     inputElement.value = null
 }
+
+
+renderNewSentence()
